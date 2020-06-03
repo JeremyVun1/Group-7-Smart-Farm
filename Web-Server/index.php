224e20c7db5744f1b049cc01e62a847c7b4aa262
Adam <100649885@student.swin.edu.au>
2020-06-03 17:47:50 +1000

    Beginning of Web Server Frontend
    - Index page
    - Canvas.js lib
    - Temp, Water, and Soil pages

--- Web-Server/index.php
+++ Web-Server/index.php
@@ -1,13 +1,62 @@
 <?php
 
+date_default_timezone_set('Australia/Melbourne');
+$params = array(
+    "start" => date('Y-m-d H:i:s', strtotime('-60 minutes'))
+);
+$params = http_build_query($params);
+
+//Get temperatures
+$handle = curl_init();
+$getTempsURL="http://localhost/Group-7-Smart-Farm/Web-Server/api/temperature/get_readings?".$params;
+curl_setopt($handle, CURLOPT_URL, $getTempsURL);
+curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+$temperatures = curl_exec($handle);
+curl_close($handle);
+
+//Get soil moistures
+$handle = curl_init();
+$getTempsURL="http://localhost/Group-7-Smart-Farm/Web-Server/api/soil/get_readings?".$params;
+curl_setopt($handle, CURLOPT_URL, $getTempsURL);
+curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+$result = curl_exec($handle);
+curl_close($handle);
+
+//Get water levels
+$handle = curl_init();
+$getTempsURL="http://localhost/Group-7-Smart-Farm/Web-Server/api/water/get_readings?".$params;
+curl_setopt($handle, CURLOPT_URL, $getTempsURL);
+curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+$result = curl_exec($handle);
+curl_close($handle);
+
 ?>
 
 <!DOCTYPE HTML>
 <html>
 <head>
+<script>
+    window.onload = function () {
 
+    var chart = new CanvasJS.Chart("chartContainer", {
+	theme: "dark2",
+  	animationEnabled: true,
+	title:{
+		text: "Email Categories",
+		horizontalAlign: "left"
+	},
+    data:  <?php echo json_encode($temperatures, JSON_NUMERIC_CHECK); ?>
+    });
+
+    chart.render();
+</script>
 </head>
 <body>
-Inital Index page
+<h1>Smart Farming Control Center</h1>
+<br><br>
+<div id="temperatureDoughnutContainer" style="height: 370px; width: 100%;"></div>
+<div id="temperatureDoughnutContainer" style="height: 370px; width: 100%;"></div>
+<div id="temperatureDoughnutContainer" style="height: 370px; width: 100%;"></div>
+<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
 </body>
 </html>
---
+++ Web-Server/temperature.php
@@ -0,0 +1,99 @@
+<?php
+
+//Get ALL the data
+$handle = curl_init();
+$getAllUrl="http://localhost/Group-7-Smart-Farm/Web-Server/api/temperature/get_readings";
+curl_setopt($handle, CURLOPT_URL, $getAllUrl);
+curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+$result = curl_exec($handle);
+curl_close($handle);
+
+$allReadings = json_decode($result);
+
+$temperatures = array();
+
+foreach($allReadings as $reading) {
+    $id = $reading->sensor_id;
+    $temperature = $reading->temperature;
+    $datetime = $reading->datetime;
+    $dataPoints = [
+        'label' => $reading->datetime,
+        'y' => $reading->temperature
+    ];
+    if(strpos(json_encode($temperatures), $id) == 0) {   //We have a new sensor
+        array_push($temperatures, 
+        [
+            'type' => "spline",
+            'showInLegend' => true,
+            'name' => $id,
+            'dataPoints' => [$dataPoints]
+        ]);
+    } else {
+        // Key alread exists, add the reading to that keys dataset
+        foreach($temperatures as &$temp) {
+            if($temp['name'] == $id) {
+                array_push($temp['dataPoints'],$dataPoints);
+            }
+        }
+    }
+}
+
+
+?>
+
+<!DOCTYPE HTML>
+<html>
+<head>
+<script>
+window.onload = function () {
+
+var chart = new CanvasJS.Chart("chartContainer", {
+	theme:"dark2",
+	animationEnabled: true,
+	title:{
+		text: "Temperatures"
+	},
+	axisY :{
+		includeZero: false,
+		title: "Temp",
+		suffix: " degrees Celcius"
+	},
+	toolTip: {
+		shared: "true"
+	},
+	legend:{
+		cursor:"pointer",
+		itemclick : toggleDataSeries
+	},
+	data: <?php echo json_encode($temperatures, JSON_NUMERIC_CHECK); ?>
+});
+chart.render();
+
+function toggleDataSeries(e) {
+	if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible ){
+		e.dataSeries.visible = false;
+	} else {
+		e.dataSeries.visible = true;
+	}
+	chart.render();
+}
+
+}    
+</script>
+</head>
+<body>
+<form method="post" action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>">
+    <h3>Please enter a datetime range you wish to search for:</h3>
+    <br>
+    Start:
+    <input type="datetime-local" name="startDatetime">
+    &nbsp; End:
+    <input type="datetime-local" name="endDatetime">
+    <br><br>
+    <input type="submit" name="search" value="Search">
+</form>
+
+<div id="chartContainer" style="height: 300px; width: 100%;"></div>
+<script src="lib/canvasjs-2.3.2/canvasjs.min.js"></script>
+</body>
+</html>
