4364f41290b379d33d13e3a5fa58187693308946
Adam <100649885@student.swin.edu.au>
2020-06-08 14:53:05 +1000

    Included Bar graphs
    - Temperature
    - Soil Moisture
    
    - Centered Text

--- Web-Server/soil.php
+++ Web-Server/soil.php
@@ -1,19 +1,39 @@
 <?php
 
-$moistures = array();
+$lineMoistures = array();
+$barMoistures = array();
+$params = array();
+$range = "";
+
+$moistureRanges = array(
+    array("label" => "0 - 20%", "y" => 0),
+    array("label" => "20 - 39%", "y" => 0),
+    array("label" => "40 - 59%", "y" => 0),
+    array("label" => "60 - 79%", "y" => 0),
+    array("label" => "80 - 100%", "y" => 0)
+);
 
 if(isset($_POST['search'])) {
     
-    $params = array();
-
     if(!empty($_POST['startDatetime'])) {
-       $start = new DateTime($_POST['startDatetime']);
-       $params += array("start" => $start->format('Y-m-d H:i:s'));
-    } 
-    if(!empty($_POST['endDatetime'])) {
-        $end = new DateTime($_POST['endDatetime']);
-        $params += array("end" => $end->format('Y-m-d H:i:s'));
-    }    
+        $start = new DateTime($_POST['startDatetime']);
+        $params += array("start" => $start->format('Y-m-d H:i:s'));
+     } 
+     if(!empty($_POST['endDatetime'])) {
+         $end = new DateTime($_POST['endDatetime']);
+         $params += array("end" => $end->format('Y-m-d H:i:s'));
+         if(isset($params['start'])) {
+             $range .= $params['start'] . "  to  " . $params['end'];
+         } else {
+             $range .= "Everything until " . $params['end']; 
+         }
+     } else {
+         if(isset($params['start'])) $range = "Everything after " . $params['start'];
+     }    
+ 
+     if(empty($_POST['startDatetime']) && empty($_POST['endDatetime'])) {
+         $range = "All Records";
+     }   
 
     $params = http_build_query($params);
     $handle = curl_init();
@@ -33,8 +53,10 @@ if(isset($_POST['search'])) {
             'label' => $reading->datetime,
             'y' => $reading->moisture_level
         ];
-        if(strpos(json_encode($moistures), $id) == 0) {   //We have a new sensor
-            array_push($moistures, 
+
+        //Build the line graph data
+        if(strpos(json_encode($lineMoistures), $id) == 0) {   //We have a new sensor
+            array_push($lineMoistures, 
             [
                 'type' => "spline",
                 'showInLegend' => true,
@@ -43,9 +65,43 @@ if(isset($_POST['search'])) {
             ]);
         } else {
             // Key alread exists, add the reading to that keys dataset
-            foreach($moistures as &$m) {
+            foreach($lineMoistures as &$m) {
                 if($m['name'] == $id) {
                     array_push($m['dataPoints'],$dataPoints);
+                }
+            }
+        }
+
+        //Build the bar graph data
+        if(strpos(json_encode($barMoistures), $id) == 0) {   //We have a new sensor
+            array_push($barMoistures, 
+            [
+                'type' => "column",
+                'showInLegend' => true,
+                'name' => $id,
+                'dataPoints' => $moistureRanges
+            ]);
+        } else {
+            // Key alread exists, add the reading to that keys dataset
+            foreach($barMoistures as &$m) {
+                if($m['name'] == $id) {
+                    switch($moisture) {
+                        case ($moisture < 500):
+                            $m['dataPoints'][0]["y"] += 1;
+                        break;
+                        case ($moisture < 650):
+                            $m['dataPoints'][1]["y"] += 1;
+                        break;
+                        case ($moisture < 800):
+                            $m['dataPoints'][2]["y"] += 1;
+                        break;
+                        case ($moisture < 950):
+                            $m['dataPoints'][3]["y"] += 1;
+                        break;
+                        case ($moisture >= 950):
+                            $m['dataPoints'][4]["y"] += 1;
+                        break;
+                    }
                 }
             }
         }
@@ -66,44 +122,72 @@ if(isset($_POST['search'])) {
 <script>
 window.onload = function () {
 
-var chart = new CanvasJS.Chart("chartContainer", {
-	theme:"dark2",
-	animationEnabled: true,
-	title:{
-		text: "Soil Moisture Readings"
-	},
-	axisY :{
-		includeZero: false,
-		title: "Moisture Level",
-		suffix: "",
-        maximum: 1024
-	},
-	toolTip: {
-		shared: "true"
-	},
-	legend:{
-		cursor:"pointer",
-		itemclick : toggleDataSeries
-	},
-	data: <?php echo json_encode($moistures, JSON_NUMERIC_CHECK); ?>
-});
-chart.render();
+    var lineChart = new CanvasJS.Chart("lineChartContainer", {
+        theme:"dark2",
+        animationEnabled: true,
+        title:{
+            text: ""
+        },
+        axisY :{
+            includeZero: false,
+            title: "Moisture Level",
+            suffix: "",
+            maximum: 1024
+        },
+        toolTip: {
+            shared: "true"
+        },
+        legend:{
+            cursor:"pointer",
+            itemclick : toggleLineDataSeries
+        },
+        data: <?php echo json_encode($lineMoistures, JSON_NUMERIC_CHECK); ?>
+    });
 
-function toggleDataSeries(e) {
-	if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible ){
-		e.dataSeries.visible = false;
-	} else {
-		e.dataSeries.visible = true;
-	}
-	chart.render();
-}
+    var barChart = new CanvasJS.Chart("barChartContainer", {
+        theme: "dark2",
+        animationEnabled: true,
+        title:{
+            text: "",
+        },
+        legend:{
+            cursor: "pointer",
+            itemclick : toggleBarDataSeries
+        },
+        data: <?php echo json_encode($barMoistures, JSON_NUMERIC_CHECK); ?> 
+    });
 
+    lineChart.render();
+    barChart.render();
+
+    function toggleLineDataSeries(e) {
+        if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible ){
+            e.dataSeries.visible = false;
+        } else {
+            e.dataSeries.visible = true;
+        }
+        lineChart.render();
+    }
+
+    function toggleBarDataSeries(e) {
+        if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible ){
+            e.dataSeries.visible = false;
+        } else {
+            e.dataSeries.visible = true;
+        }
+        barChart.render();
+    }
 }    
 </script>
 </head>
 <body>
+<h1>Soil Moisture Readings</h1>
+<br>
+<h2 style="display:<?php echo $display;?>;">Search Range: <?php echo $range; ?></h2>
 
-<div id="chartContainer" style="display:<?php echo $display;?>; height: 300px; width: 100%;"></div>
+<div id="lineChartContainer" style="display:<?php echo $display;?>; height: 300px; width: 100%;"></div>
+<br><br>
+<div id="barChartContainer" style="display:<?php echo $display;?>; height: 300px; width: 100%;"></div>
 
 <form method="post" action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>">
     <h3>Please enter a datetime range you wish to search for:</h3>
--- Web-Server/temperature.php
+++ Web-Server/temperature.php
@@ -1,19 +1,46 @@
 <?php
 
-$temperatures = array();
+$lineTemperatures = array();
+$barTemperatures = array();
+$params = array();
+$range = "";
+
+$temperatureRanges = array(
+    array("label" => "-0°C", "y" => 0),
+    array("label" => "0-4.99°C", "y" => 0),
+    array("label" => "5-9.99°C", "y" => 0),
+    array("label" => "10-14.99°C", "y" => 0),
+    array("label" => "15-19.99°C", "y" => 0),
+    array("label" => "20-24.99°C", "y" => 0),
+    array("label" => "25-29.99°C", "y" => 0),
+    array("label" => "30-34.99°C", "y" => 0),
+    array("label" => "35-39.99°C", "y" => 0),
+    array("label" => "40-44.99°C", "y" => 0),
+    array("label" => "45-49.99°C", "y" => 0),
+    array("label" => "50°C+", "y" => 0)
+);
 
 if(isset($_POST['search'])) {
-    
-    $params = array();
 
     if(!empty($_POST['startDatetime'])) {
-       $start = new DateTime($_POST['startDatetime']);
-       $params += array("start" => $start->format('Y-m-d H:i:s'));
-    } 
-    if(!empty($_POST['endDatetime'])) {
-        $end = new DateTime($_POST['endDatetime']);
-        $params += array("end" => $end->format('Y-m-d H:i:s'));
-    }    
+        $start = new DateTime($_POST['startDatetime']);
+        $params += array("start" => $start->format('Y-m-d H:i:s'));
+     } 
+     if(!empty($_POST['endDatetime'])) {
+         $end = new DateTime($_POST['endDatetime']);
+         $params += array("end" => $end->format('Y-m-d H:i:s'));
+         if(isset($params['start'])) {
+             $range .= $params['start'] . "  to  " . $params['end'];
+         } else {
+             $range .= "Everything until " . $params['end']; 
+         }
+        } else {
+            if(isset($params['start'])) $range = "Everything after " . $params['start'];
+        }     
+ 
+     if(empty($_POST['startDatetime']) && empty($_POST['endDatetime'])) {
+         $range = "All Records";
+     }
 
     $params = http_build_query($params);
     $handle = curl_init();
@@ -25,8 +52,8 @@ if(isset($_POST['search'])) {
 
     $readings = json_decode($result);
 
-
     foreach($readings as $reading) {
+        
         $id = $reading->sensor_id;
         $temperature = $reading->temperature;
         $datetime = $reading->datetime;
@@ -34,8 +61,9 @@ if(isset($_POST['search'])) {
             'label' => $reading->datetime,
             'y' => $reading->temperature
         ];
-        if(strpos(json_encode($temperatures), $id) == 0) {   //We have a new sensor
-            array_push($temperatures, 
+        //Build the line graph data
+        if(strpos(json_encode($lineTemperatures), $id) == 0) {   //We have a new sensor
+            array_push($lineTemperatures, 
             [
                 'type' => "spline",
                 'showInLegend' => true,
@@ -44,9 +72,64 @@ if(isset($_POST['search'])) {
             ]);
         } else {
             // Key alread exists, add the reading to that keys dataset
-            foreach($temperatures as &$temp) {
+            foreach($lineTemperatures as &$temp) {
                 if($temp['name'] == $id) {
                     array_push($temp['dataPoints'],$dataPoints);
+                }
+            }
+        }
+
+        //Build the bar graph data
+        if(strpos(json_encode($barTemperatures), $id) == 0) {   //We have a new sensor
+            array_push($barTemperatures, 
+            [
+                'type' => "column",
+                'showInLegend' => true,
+                'name' => $id,
+                'dataPoints' => $temperatureRanges
+            ]);
+        } else {
+            // Key alread exists, add the reading to that keys dataset
+            foreach($barTemperatures as &$temp) {
+                if($temp['name'] == $id) {
+                    switch($temperature) {
+                        case ($temperature < 0):
+                            $temp['dataPoints'][0]["y"] += 1;
+                        break;
+                        case ($temperature < 5):
+                            $temp['dataPoints'][1]["y"] += 1;
+                        break;
+                        case ($temperature < 10):
+                            $temp['dataPoints'][2]["y"] += 1;
+                        break;
+                        case ($temperature < 15):
+                            $temp['dataPoints'][3]["y"] += 1;
+                        break;
+                        case ($temperature < 20):
+                            $temp['dataPoints'][4]["y"] += 1;
+                        break;
+                        case ($temperature < 25):
+                            $temp['dataPoints'][5]["y"] += 1;
+                        break;
+                        case ($temperature < 30):
+                            $temp['dataPoints'][6]["y"] += 1;
+                        break;
+                        case ($temperature < 35):
+                            $temp['dataPoints'][7]["y"] += 1;
+                        break;
+                        case ($temperature < 40):
+                            $temp['dataPoints'][8]["y"] += 1;
+                        break;
+                        case ($temperature < 45):
+                            $temp['dataPoints'][9]["y"] += 1;
+                        break;
+                        case ($temperature < 50):
+                            $temp['dataPoints'][10]["y"] += 1;
+                        break;
+                        case ($temperature >= 50):
+                            $temp['dataPoints'][11]["y"] += 1;
+                        break;
+                    }
                 }
             }
         }
@@ -66,14 +149,13 @@ if(isset($_POST['search'])) {
 <script src="lib/canvasjs-2.3.2/canvasjs.min.js"></script>
 <script>
 window.onload = function () {
-    var chart = new CanvasJS.Chart("chartContainer", {
+    var lineChart = new CanvasJS.Chart("lineChartContainer", {
         theme:"dark2",
         animationEnabled: true,
         title:{
-            text: "Temperatures"
+            text: ""
         },
         axisY :{
-            includeZero: false,
             title: "Temp",
             suffix: "°C"
         },
@@ -81,27 +163,56 @@ window.onload = function () {
             shared: "true"
         },
         legend:{
-            cursor:"pointer",
-            itemclick : toggleDataSeries
+            cursor: "pointer",
+            itemclick : toggleLineDataSeries
         },
-        data: <?php echo json_encode($temperatures, JSON_NUMERIC_CHECK); ?>
+        data: <?php echo json_encode($lineTemperatures, JSON_NUMERIC_CHECK); ?>
     });
-    chart.render();
 
-    function toggleDataSeries(e) {
+    var barChart = new CanvasJS.Chart("barChartContainer", {
+        theme: "dark2",
+        animationEnabled: true,
+        title:{
+            text: "",
+        },
+        legend:{
+            cursor: "pointer",
+            itemclick : toggleBarDataSeries
+        },
+        data: <?php echo json_encode($barTemperatures, JSON_NUMERIC_CHECK); ?> 
+    });
+
+    lineChart.render();
+    barChart.render();
+
+    function toggleLineDataSeries(e) {
         if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible ){
             e.dataSeries.visible = false;
         } else {
             e.dataSeries.visible = true;
         }
-        chart.render();
+        lineChart.render();
+    }
+
+    function toggleBarDataSeries(e) {
+        if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible ){
+            e.dataSeries.visible = false;
+        } else {
+            e.dataSeries.visible = true;
+        }
+        barChart.render();
     }
 }    
 </script>
 </head>
 <body>
+<h1>Temperatures</h1>
+<br>
+<h2 style="display:<?php echo $display;?>;">Search Range: <?php echo $range; ?></h2>
 
-<div id="chartContainer" style="display:<?php echo $display;?>; height: 300px; width: 100%;"></div>
+<div id="lineChartContainer" style="display:<?php echo $display;?>; height: 300px; width: 100%;"></div>
+<br><br>
+<div id="barChartContainer" style="display:<?php echo $display;?>; height: 300px; width: 100%;"></div>
 
 <form method="post" action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>">
     <h3>Please enter a datetime range you wish to search for:</h3>
