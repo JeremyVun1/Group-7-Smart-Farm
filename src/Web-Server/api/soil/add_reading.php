387e9d302c889c7c946c64cebbc58a8659e0ebfa
Jeremy Vun <2726092@student.swin.edu.au>
2020-06-11 21:14:48 +1000

    attempt fix to adding moisture value

--- src/Web-Server/api/soil/add_reading.php
+++ src/Web-Server/api/soil/add_reading.php
@@ -9,6 +9,7 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 
 include_once "../../config/database.php";
 include_once "../../objects/soil.php";
+include_once "../../objects/config.php";
 
 include '../../lib/phpMQTTpub.php';
 
