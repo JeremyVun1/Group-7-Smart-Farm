c6164e704d90124cd744cf32885c4690124e9bdf
Jeremy Vun <2726092@student.swin.edu.au>
2020-06-12 10:38:50 +1000

    fix header

--- src/Web-Server/options.php
+++ src/Web-Server/options.php
@@ -43,23 +43,25 @@
 </head>
 <body>
     <!-- HEADER -->
-    <nav class="navbar navbar-light bg-light">
-        
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item active">
-                        <a href="/">
-                            <span class="navbar-brand mb-0 h1">
-                                <img src="./static/icon.png" />
-                                Smart Farm Dashboard
-                            </span>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="options.php">Options</a>
-                    </li>
-                </ul>
-            </div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a href="/">
+            <span class="navbar-brand mb-0 h1">
+                <img src="./static/icon.png" />
+                Smart Farm Dashboard
+            </span>
+        </a>
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="options.php">Options</a>
+                </li>
+            </ul>
+        </div>
         <span class="navbar-brand mb-0 h1">Group 7 - SWE-30011 IoT Programming</span>
     </nav>
 
